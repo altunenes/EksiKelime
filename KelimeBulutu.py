e88a7d68e69767d3766e6e8246c94ec97b962909
Enes <54986652+altunenes@users.noreply.github.com>
2021-10-30 12:48:17 +0300

    Create KelimeBulutu.py

---
+++ KelimeBulutu.py
@@ -0,0 +1,106 @@
+"""
+# ==========================================================================================================================================================
+#                                                                       Yazar: Enes Altun
+# ==========================================================================================================================================================
+"""
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+import string
+
+
+"""
+# ==========================================================================================================================================================
+#                                             Sadece aşağıdaki "yazar.csv" bölümüne, çalışma dizininizde bulunan csv dosyanızın ismini yazın
+# ==========================================================================================================================================================
+"""
+
+df=pd.read_csv("yazar.csv")
+
+sent=pd.read_csv("turkish_lexicon.csv",encoding="utf-8-sig")
+
+stop=pd.read_csv("edatbag.csv")
+
+
+
+def remove_punctuation(text):
+    punctuationfree="".join([i for i in text if i not in string.punctuation])
+    return punctuationfree
+
+
+
+df['entry']= df['entry'].apply(lambda x:remove_punctuation(x))
+#ing
+stopWordsListEng = stopwords.words("english")
+_new_stopwords_to_add=stop["kelime"]
+stopWordsListEng.extend(_new_stopwords_to_add)
+df['entry'] = df['entry'].apply(lambda x: ' '.join([item for item in x.split() if item not in stopWordsListEng]))
+df['entry']= df['entry'].apply(lambda x: x.lower())
+
+
+
+
+def word_frequency(df):
+    #create a list of all the words in the dataframe
+    word_list = []
+    for i in range(len(df)):
+        word_list.append(df.iloc[i][0])
+    #create a list of all the unique words in the dataframe
+    unique_word_list = []
+    for i in range(len(word_list)):
+        if word_list[i] not in unique_word_list:
+            unique_word_list.append(word_list[i])
+    #create a dictionary of the frequency of each word
+    word_frequency_dict = {}
+    for i in range(len(unique_word_list)):
+        word_frequency_dict[unique_word_list[i]] = word_list.count(unique_word_list[i])
+    #create a dataframe of the word frequency dictionary
+    word_frequency_df = pd.DataFrame(word_frequency_dict, index = [0])
+    word_frequency_df = word_frequency_df.T
+    word_frequency_df.columns = ['frequency']
+    #sort the dataframe by the frequency of each word
+    word_frequency_df = word_frequency_df.sort_values(by = 'frequency', ascending = True)
+    return word_frequency_df
+
+
+def get_word_frequency(df, column):
+    #create a list of all the words in the phrase
+    words = []
+    for i in range(len(df)):
+        words.append(df[column][i].split())
+    #create a list of all the unique words in the phrase
+    unique_words = []
+    for i in range(len(words)):
+        for j in range(len(words[i])):
+            if words[i][j] not in unique_words:
+                unique_words.append(words[i][j])
+    #create a dictionary of the frequency of each unique word in the phrase
+    word_frequency = {}
+    for i in range(len(unique_words)):
+        word_frequency[unique_words[i]] = 0
+    for i in range(len(words)):
+        for j in range(len(words[i])):
+            word_frequency[words[i][j]] += 1
+    return word_frequency
+
+
+
+#data + sütun inputu
+
+word_frequency=get_word_frequency(df,"entry")
+from wordcloud import WordCloud
+
+
+
+def plot_word_cloud(wordfrequency):
+    wordcloud = WordCloud(width = 1000, height = 500).generate_from_frequencies(wordfrequency)
+    plt.figure(figsize = (15,8))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show()
+
+
+plot_word_cloud(word_frequency)
